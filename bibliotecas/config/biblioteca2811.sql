e3816659bf870ab4f9787a3f14a62e155ae73c89
Carlos Eduardo De L. G. <cadulucasgonzalez@gmail.com>
2024-12-01 22:55:13 -0300

    acaba meu deus

--- bibliotecas/config/biblioteca2811.sql
+++ bibliotecas/config/biblioteca2811.sql
@@ -1,28 +1,30 @@
 create database biblioteca;
 use biblioteca;
 
-create table usuario(
+create table if not exists usuario(
 	id_usuario int primary key auto_increment,
     nome varchar(100),
+    email varchar(30),
     cpf varchar(13),
-    telefone varchar(20)
+    senha varchar(15),
+    administrador boolean default false
 );
 
-
-create table livro (
+create table if not exists livro(
  id_livro int auto_increment primary key,
- titulo varchar(50),
- autor varchar(50),
+ titulo varchar(255),
+ autor varchar(255),
+ isbn varcahr(13) unique not null,
  genero varchar(50),
- status varchar(30),
- codigo int
+ status varchar(255) default 'disponível'
 );
 
-create table emprestimo(
+create table if not exists emprestimo(
 	id_emprestimo int auto_increment primary key,
-    id_livro int,
-    id_usuario int,
-     foreign key (id_livro) references livro(id_livro),
-	 foreign key (id_usuario) references usuario(id_usuario)
-
+    id_livro int not null,
+    id_usuario int not null,
+    data_emprestimo date default current_date,
+    data_devolução date,
+    foreign key (id_livro) references livro(id_livro),
+	foreign key (id_usuario) references usuario(id_usuario)
 );
